9c30d2b0f793d8160368e814238436a2c57c2213
Ian Hogg <ian.w.hogg@virginmedia.com>
2022-12-15 16:31:48 +0000

    Fix for end of pulsed output event having wrong polarity

--- canmio.h
+++ canmio.h
@@ -69,8 +69,8 @@ extern "C" {
 #define BETA        0           // BETA version 0=non beta release
 #else
 #define MAJOR_VER 	3
-#define MINOR_VER 	'c'        // Minor version character
-#define BETA        0           // BETA version 0=non beta release
+#define MINOR_VER 	'd'        // Minor version character
+#define BETA        1           // BETA version 0=non beta release
 #endif
 
 #include "GenericTypeDefs.h"
